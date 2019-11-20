d43db6e25a30b2e0610ef3f3de2dff88bec61dc5
Altanbagana92 <57874164+Altanbagana92@users.noreply.github.com>
2019-11-20 14:45:27 +0100

    Update measure-disk-random-read.sh

--- measure-disk-random-read.sh
+++ measure-disk-random-read.sh
@@ -5,5 +5,5 @@
 WRITE=$(fio --thread --randrepeat=1 --ioengine=libaio --direct=1 --gtod_reduce=1 --name=test --filename=test --bs=4k --iodepth=64 --runtime=7 --readwrite=randwrite --minimal --size=4G | cut -d ";" -f 49)
 # random read
 READ=$(fio --thread --randrepeat=1 --ioengine=libaio --direct=1 --gtod_reduce=1 --name=test --filename=test --bs=4k --iodepth=64 --runtime=7 --readwrite=randread --minimal --size=4G | cut -d ";" -f 8)
-#rm test
+rm test
 echo "$READ" | awk '{printf $1"\n"}'
